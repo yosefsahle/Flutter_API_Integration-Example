b3624bd3e47d9e3795302f975c1a4977a7326bef
yosefsahle <yosefsahle48@gmail.com>
2024-01-12 15:49:28 +0300

    Flutter Api Integration Example

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.white,
+          textTheme: TextTheme(
+            bodyMedium: TextStyle(color: Colors.white),
+          )),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:sampleapp/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "33e2180175msh2bfb086961807dap198b40jsn8ab087ef440e",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
