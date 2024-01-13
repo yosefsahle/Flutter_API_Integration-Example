5c36bdd00cd7d0550d62a041dba6f281663fd458
yosefsahle <yosefsahle48@gmail.com>
2024-01-13 15:17:18 +0300

    change

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -28,6 +28,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(),
         appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
