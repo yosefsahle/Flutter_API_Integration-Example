6f281fb07012eeae353888112a08cd1fcd603b48
yosefsahle <yosefsahle48@gmail.com>
2024-01-13 15:19:40 +0300

    Books

--- lib/views/book.dart
+++ lib/views/book.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class BookPage extends StatelessWidget {
+  const BookPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
